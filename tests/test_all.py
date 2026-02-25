090dabd66854985c651b1143269de9558a851b6f
ekanant1412 <ekanant.chanchuey@gmail.com>
2026-02-25 13:55:54 +0700

    Add test_DMPREC_9588 to test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,6 +2,7 @@ from src.DMPREC_9584 import test_DMPREC_9584
 from src.DMPREC_9585 import test_DMPREC_9585
 from src.DMPREC_9586 import test_DMPREC_9586
 from src.DMPREC_9587 import test_DMPREC_9587
+from src.DMPREC_9588 import test_DMPREC_9588
 from src.DMPREC_9589 import test_DMPREC_9589
 from src.DMPREC_9590 import test_DMPREC_9590
 from src.DMPREC_9591 import test_DMPREC_9591
