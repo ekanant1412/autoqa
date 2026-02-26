df270599fa5bddac7dfd989a2b274c0cd8757f37
ekanant1412 <ekanant.chanchuey@gmail.com>
2026-02-27 03:16:27 +0700

    Update test imports in test_all.py
    Comment out existing test imports and add new test imports.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,10 +1,13 @@
-from src.DMPREC_9584 import test_DMPREC_9584
-from src.DMPREC_9585 import test_DMPREC_9585
-from src.DMPREC_9586 import test_DMPREC_9586
-from src.DMPREC_9587 import test_DMPREC_9587
-from src.DMPREC_9588 import test_DMPREC_9588
-from src.DMPREC_9589 import test_DMPREC_9589
-from src.DMPREC_9590 import test_DMPREC_9590
-from src.DMPREC_9613 import test_DMPREC_9613
-from src.DMPREC_9700 import test_DMPREC_9700
-from src.DMPREC_9701 import test_DMPREC_9701
+# from src.DMPREC_9584 import test_DMPREC_9584
+# from src.DMPREC_9585 import test_DMPREC_9585
+# from src.DMPREC_9586 import test_DMPREC_9586
+# from src.DMPREC_9587 import test_DMPREC_9587
+# from src.DMPREC_9588 import test_DMPREC_9588
+# from src.DMPREC_9589 import test_DMPREC_9589
+# from src.DMPREC_9590 import test_DMPREC_9590
+# from src.DMPREC_9613 import test_DMPREC_9613
+# from src.DMPREC_9700 import test_DMPREC_9700
+# from src.DMPREC_9701 import test_DMPREC_9701
+
+from src.DMPREC_9706 import test_DMPREC_9706
+from src.DMPREC_9707 import test_DMPREC_9707
